ecae723e81d0fbf28cb1909fd81e087469987759
Md Saeem Hossain Shanto <Saeem.Shanto@northsouth.edu>
2023-03-10 22:48:39 +0600

    index.html

--- ACM_Django/warehouse/urls.py
+++ ACM_Django/warehouse/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path,include
 from . import views
 urlpatterns = [
-    path("",views.home,name="index")
+    path("",views.index,name="index")
 ]
 
--- ACM_Django/warehouse/views.py
+++ ACM_Django/warehouse/views.py
@@ -8,6 +8,11 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 
+def index(request):
+    return render(request,"index.html",{})
+
+
+
 @api_view(['GET', 'POST'])
 @csrf_exempt 
 def home(request):
